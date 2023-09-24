be733be29957ffd1a2f90acaa99bc3395fe92adb
Shyam Kumar <contactskchauhan@gmail.com>
2023-09-24 23:29:10 +0530

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AskQuestion() {
+  return <div>Ask Questions</div>;
+}
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJs 13 we are comming...</h1>
-      <h1 className="h2-bold">NextJs 13 we are comming...</h1>
-      <h1 className="h3-bold">NextJs 13 we are comming...</h1>
-    </div>
-  );
-}
